20ee19b3e6ad6aebbb5879dca753e6a1655fc0eb
aryan008 <adam.s.ryan@pwc.com>
2021-04-23 19:18:24 +0000

    feat: add the audio JS file and audio function

---
+++ assets/js/audio.js
@@ -0,0 +1,7 @@
+
+// Understanding of how to add audio at the following source:
+// https://www.codegrepper.com/code-examples/javascript/how+to+add+sound+javascript
+function audioHand() {
+  var audio = new Audio("assets/audio/rps_shoot.m4a");
+  audio.play();
+}

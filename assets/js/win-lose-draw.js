53d4fbc4cc51fbda810d6914e47712fb958e15df
aryan008 <adam.s.ryan@pwc.com>
2021-04-23 18:48:19 +0000

    feat: add the lose function, copied from win function previous and changed appropriately

--- assets/js/win-lose-draw.js
+++ assets/js/win-lose-draw.js
@@ -20,3 +20,25 @@ function win(userHand, cpu) {
         }, 500);
     }
 }
+
+function lose(userHand, cpu) {
+    $("#result-text-paragraph").html(`<h4>You lost :( You chose ${userHand} but the computer chose ${cpu}, hard luck!<span class = "result-emoji">&#128549;</span></h4>`
+    );
+    incrementLoseScore();
+    if (userHand === "rock") {
+        $("#rockHand").addClass("red-click-lose");
+        setTimeout(function(){
+            $("#rockHand").removeClass("red-click-lose");
+        }, 500);
+    } else if (userHand === "paper") {
+        $("#paperHand").addClass("red-click-lose");
+        setTimeout(function(){
+            $("#paperHand").removeClass("red-click-lose");
+        }, 500);
+    } else if (userHand === "scissors") {
+        $("#scissorsHand").addClass("red-click-lose");
+        setTimeout(function(){
+            $("#scissorsHand").removeClass("red-click-lose");
+        }, 500);
+    }
+}

41e98551148f18567b7f27ae1f021935d94b4305
aryan008 <adam.s.ryan@pwc.com>
2021-04-23 17:18:58 +0000

    feat: add the play function and switch statements for user v cpu

--- assets/js/gameplay.js
+++ assets/js/gameplay.js
@@ -1,3 +1,25 @@
+function play(userHand) {
+    var cpu = computerHand();    
+        
+    switch(userHand + "-" + cpu) {
+        case "rock-scissors":
+        case "scissors-paper":
+        case "paper-rock":
+            win(userHand, cpu);
+            break;
+        case "rock-paper":
+        case "scissors-rock":
+        case "paper-scissors":
+            lose(userHand, cpu);
+            break;
+        case "rock-rock":
+        case "scissors-scissors":
+        case "paper-paper":
+            draw(userHand, cpu);
+            break;
+    }
+}
+
 function main() {
     $("#rockHand").on("click", function () {
         play("rock");

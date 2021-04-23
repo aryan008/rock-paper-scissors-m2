caa3f3be778c8028ca513c3d22eae4f477dbf608
aryan008 <adam.s.ryan@pwc.com>
2021-04-23 16:17:26 +0000

    feat: caching the read more/less buttons in JS

--- assets/js/initialisation.js
+++ assets/js/initialisation.js
@@ -1,3 +1,5 @@
 const rockHand = document.getElementById("rockHand");
 const paperHand = document.getElementById("paperHand");
 const scissorsHand = document.getElementById("scissorsHand");
+const readMore = document.getElementById("btn--read--more");
+const readLess = document.getElementById("btn--read--less");

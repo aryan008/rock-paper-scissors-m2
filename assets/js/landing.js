feacc62c9547d76b29f9bef7e3d008250c27957b
aryan008 <adam.s.ryan@pwc.com>
2021-04-23 16:25:49 +0000

    feat: hiding the appropriate content on document load

---
+++ assets/js/landing.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $(playButton).hide();
+    $("#picture-div").hide();
+    $("#play-div").hide();
+    $(".overall-div").hide();
+    $(".game-count-choice").hide().fadeIn()
+});

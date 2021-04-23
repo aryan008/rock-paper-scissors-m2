14e3a246a23105e9d3b8d123baac0b2c3b98db43
aryan008 <adam.s.ryan@pwc.com>
2021-04-23 16:34:33 +0000

    feat: add the loading screen onclick event in JS

--- assets/js/game-launch.js
+++ assets/js/game-launch.js
@@ -1,4 +1,10 @@
 $(".game-number-button").click(function() {
     $("#game--count--choice--div").fadeOut(200);
     $(playButton).delay(200).fadeIn(600);
-})
+});
+
+$("#btn--play--click").click(function() {
+    $("#intro-div").fadeOut();
+    $("#picture-div").delay().fadeIn().fadeOut();
+    $("#play-div").delay().fadeIn();
+});

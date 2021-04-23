927db3390b86b748181bb39f40bca2afc9c8d1d9
aryan008 <adam.s.ryan@pwc.com>
2021-04-23 18:15:45 +0000

    feat: add the win function to display text and increment the score

--- assets/js/win-lose-draw.js
+++ assets/js/win-lose-draw.js
@@ -1,6 +1,6 @@
-/*
 function win(userHand, cpu) {
-    resultText.innerHTML = `
-    <h4>You won! You chose ${userHand} and the computer chose ${cpu}, congratulations!<span class = "result-emoji">&#128516;</span></h4>`;
+    $("#result-text-paragraph").html(`
+    <h4>You won! You chose ${userHand} and the computer chose ${cpu}, congratulations!<span class = "result-emoji">&#128516;</span></h4>`
+    );
     incrementWinScore();
-}*/
+}

3822675b268d17eff144e9541dfc905f52b21940
aryan008 <adam.s.ryan@pwc.com>
2021-04-23 17:52:20 +0000

    feat: duplicate win function and change around for a loss

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -7,7 +7,32 @@ function incrementWinScore() {
         $("#player-score").classList.remove("green-flash");
     }, 500);
 
-    if (computerScore.innerHTML === gameChoice) {
+    if ($("#player-score").innerHTML === gameChoice) {
+        $("#overall-div").show().fadeOut(4000);
+        // Trophy obtained at the following source: https://www.compart.com/en/unicode/U+1F44F
+        $("#overall-winner").show().html(`
+        Congrats, you were first to ${gameChoice}!
+        <br>
+        <span class = "span-trophy">&#127942;</span>
+        <br>
+        <h2 id = "overall-winner">Let's play again!</h2>
+        `).fadeOut(4000);
+        $("#game-div").hide().delay(4000).fadeIn(1500);
+        $("#btn--game--controls").hide().delay(4000).fadeIn(1500);
+        resetFunc();
+    }
+}
+
+function incrementLoseScore() {
+    $("#cpu-score").innerHTML = ++cpuScore;
+    $("#cpu-score-text").classList.add("red-flash");
+    $("#cpu-score").classList.add("red-flash");
+    setTimeout(function(){
+        $("#cpu-score-text").classList.remove("red-flash");
+        $("#cpu-score").classList.remove("red-flash");
+    }, 500);
+
+    if ($("#cpu-score").innerHTML === gameChoice) {
         $("#overall-div").show().fadeOut(4000);
         // Trophy obtained at the following source: https://www.compart.com/en/unicode/U+1F44F
         $("#overall-winner").show().html(`
@@ -15,7 +40,7 @@ function incrementWinScore() {
         <br>
         <span class = "span-trophy">&#128079;&#127996;</span>
         <br>
-        <h2 id = "overall-winner">Better luck on the next game!</h2>
+        <h2 id = "overall-winner">Let's play again!</h2>
         `).fadeOut(4000);
         $("#game-div").hide().delay(4000).fadeIn(1500);
         $("#btn--game--controls").hide().delay(4000).fadeIn(1500);

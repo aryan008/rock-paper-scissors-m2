8bb3a7e144afcae1818a2a7f10b84c75353755ed
aryan008 <adam.s.ryan@pwc.com>
2021-04-23 18:18:31 +0000

    feat: set up of the appendation of win colouring to the Win hand

--- assets/js/win-lose-draw.js
+++ assets/js/win-lose-draw.js
@@ -3,4 +3,20 @@ function win(userHand, cpu) {
     <h4>You won! You chose ${userHand} and the computer chose ${cpu}, congratulations!<span class = "result-emoji">&#128516;</span></h4>`
     );
     incrementWinScore();
+    if (userHand === "rock") {
+        $("#rockHand").addClass("green-click-win");
+        setTimeout(function(){
+            $("#rockHand").removeClass("green-click-win");
+        }, 500);
+    } else if (userHand === "paper") {
+        $("#paperHand").addClass("green-click-win");
+        setTimeout(function(){
+            $("#paperHand").removeClass("green-click-win");
+        }, 500);
+    } else if (userHand === "scissors") {
+        $("#scissorsHand").addClass("green-click-win");
+        setTimeout(function(){
+            $("#scissorsHand").removeClass("green-click-win");
+        }, 500);
+    }
 }

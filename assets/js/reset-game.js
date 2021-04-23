226febb3ca2fcfa19b57ba2ed63c48e4e8f03230
aryan008 <adam.s.ryan@pwc.com>
2021-04-23 17:00:29 +0000

    refactor: update for jQuery references in reset game function

--- assets/js/reset-game.js
+++ assets/js/reset-game.js
@@ -2,7 +2,7 @@ $("#btn--new--game").on("click", resetFunc);
 function resetFunc() {
     userScore = 0;
     cpuScore = 0;
-    playerScore.innerHTML = 0;
-    computerScore.innerHTML = 0;
-    resultText.innerHTML = "";
+    $("#player-score").innerHTML = 0;
+    $("#cpu-score").innerHTML = 0;
+    $("#result-text-paragraph").innerHTML = "";
 }

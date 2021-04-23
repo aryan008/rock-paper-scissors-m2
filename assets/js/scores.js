8662b088b3dbc55a1882d093120a19d6a3929268
aryan008 <adam.s.ryan@pwc.com>
2021-04-23 17:32:23 +0000

    style: set timeout function added to remove green flash on win

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -2,4 +2,8 @@ function incrementWinScore() {
     $("#player-score").innerHTML = ++userScore;
     $("#player-score-text").classList.add("green-flash");
     $("#player-score").classList.add("green-flash");
+    setTimeout(function(){
+        $("#player-score-text").classList.remove("green-flash");
+        $("#player-score").classList.remove("green-flash");
+    }, 500);
 }

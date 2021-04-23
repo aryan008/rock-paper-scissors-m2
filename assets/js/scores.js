9c49ad92abefef88952d924dbf7ead2cea0ebc8d
aryan008 <adam.s.ryan@pwc.com>
2021-04-23 17:30:55 +0000

    style: apply green flash on a win/ adding colourful css styles to style.css

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,3 +1,5 @@
 function incrementWinScore() {
     $("#player-score").innerHTML = ++userScore;
+    $("#player-score-text").classList.add("green-flash");
+    $("#player-score").classList.add("green-flash");
 }

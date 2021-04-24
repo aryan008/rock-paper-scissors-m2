3b947a5def3f4a188d54cd5fdfe6450c1221776a
aryan008 <adam.s.ryan@pwc.com>
2021-04-24 14:14:11 +0000

    docs: add documentation to history.js file

--- assets/js/game-launch.js
+++ assets/js/game-launch.js
@@ -8,27 +8,27 @@
 Function noted in other files: resetFunc()
 */
 
-/** This onclick function will occur when the user picks the game number they want to play */
+/** This onclick event function will occur when the user picks the game number they want to play */
 $(".game-number-button").click(function() {
-    // fading out of div
+    // fading out of div for 200ms
     $("#game--count--choice--div").fadeOut(200);
-    // fading in of div
+    // fading in of div after 200ms for 600ms
     $("#div--btn--play--heading").delay(200).fadeIn(600);
 });
 
-/** This onclick function will occur when the user clicks that they are ready to play */
+/** This onclick event function will occur when the user clicks that they are ready to play */
 $("#btn--play--click").click(function() {
-    // fading out of div
+    // fading out of div for 300ms
     $("#introduction-div").fadeOut(300);
-    // fading in of div
+    // fading in of div after 500ms for 2000ms and fading out for 2000ms
     $("#picture-div").delay(500).fadeIn(2000).fadeOut(2000);
-    // fading in of div
+    // fading in of div after 5000ms for 1000ms
     $("#play-div").delay(5000).fadeIn(1000);
     // Adding resetFunc() function to the click event
     resetFunc();
 });
 
-/** This onclick function updates the "gameChoice" variable for the object ID game choice chosen by the user */
+/** This onclick event function updates the "gameChoice" variable for the object ID game choice chosen by the user */
 $(".game-number-button").on("click", function(e) {
     // setting the gameChoice variable of the object id of the user's chosen game --> score to be obtained
     gameChoice = $(e.target)[0].id;
--- assets/js/history.js
+++ assets/js/history.js
@@ -1,4 +1,17 @@
+/**
+* @fileOverview JavaScript History of the game Library.
+* @author <a href="https://github.com/aryan008">Adam Ryan</a>
+* @version 1.1.1
+*/
+/*jshint esversion: 6 */
+
+/** This onclick event function will occur when the user clicks on the read more button displayed on the landing page.
+ * @overview Adds the history text obtained by the Wikipedia source to the web page using Jquery html method.
+ * @source Wikipedia - links included in the text on the click and so visible by the user
+ */
 $("#btn--read--more").click(function() {
+    // adds the correct text to the chosen div
+    // id's, break lines and classes are added in the html method and are targeted by CSS styling rules
     $("#history-div").html(`
     <p id = "history-text">Rock paper scissors is a hand game usually played between two people, in which each player simultaneously forms one of three shapes with an outstretched hand. These shapes are "rock" (a closed fist), "paper" (a flat hand), and "scissors" (a fist with the index finger and middle finger extended, forming a V).
     <br>
@@ -16,6 +29,8 @@ $("#btn--read--more").click(function() {
     `);
 });
 
+/**This event handler function removes the history of the game text */
 $("#btn--read--less").click(function() {
+    // targets the previos history texts and removes from view
     $("#history-text").remove();
 });

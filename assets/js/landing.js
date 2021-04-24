3b201cc445c0c6d5e50d591a1525236519670553
aryan008 <adam.s.ryan@pwc.com>
2021-04-24 14:17:16 +0000

    docs: add documentation to landing.js file

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,7 +1,20 @@
+/**
+* @fileOverview JavaScript document loading Library.
+* @author <a href="https://github.com/aryan008">Adam Ryan</a>
+* @version 1.1.1
+*/
+/*jshint esversion: 6 */
+
+/**Event handler function to perform multiple functions when document is ready */
 $(document).ready(function() {
+    // hide the div
     $("#div--btn--play--heading").hide();
+    // hide the div
     $("#picture-div").hide();
+    // hide the div
     $("#play-div").hide();
+    // hide the div
     $("#overall-div").hide();
+    // Show the div
     $("#game--count--choice--div").show();
 });
